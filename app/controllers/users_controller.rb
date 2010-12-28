f59a366896d16041e04a9147900880acf261206a
Daniel Bower <daniel.bower@ecommerce.com>
2010-12-28 16:32:07 -0500

    need to add a redirect if no user found?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   def show
     if(current_user)
       @places = Place.places_for_user current_user
+    else
+      # redirect to login screen
     end
   end
 end
