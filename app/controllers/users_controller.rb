c92f8fe1eae03e4380766bcb13174cfc989741f2
Daniel Bower <daniel.bower@ecommerce.com>
2010-12-28 16:34:25 -0500

    rollback previous - login already displayed on users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,6 @@ class UsersController < ApplicationController
   def show
     if(current_user)
       @places = Place.places_for_user current_user
-    else
-      # redirect to login screen
     end
   end
 end
