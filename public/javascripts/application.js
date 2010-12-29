782a4c37c5b6593aaa40d19061e3695072d736e9
Daniel Bower <daniel.bower@ecommerce.com>
2010-12-29 12:54:57 -0500

    First implementation of hashing

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,24 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-function hashit(placename){
-  $('hashedPass').firstChild.nodeValue = $('master').value + placename
+function hashit(placename, times, encodingChars){
+  //pull the master password and the place
+  toHash = $('master').value + placename
+
+  //encode to utf8
+  hashed = str2rstr_utf8(toHash)
+
+  //hash the specified number of times
+  for (i=0; i<times; i++) {
+    hashed = rstr_md5(hashed)
+  }
+
+  //encode to hex
+  encoded = rstr2any(hashed, encodingChars)
+
+  //set field on page
+  $('hashedPass').firstChild.nodeValue = encoded
+
+  //give focus to field and highlight text?
 }
 
