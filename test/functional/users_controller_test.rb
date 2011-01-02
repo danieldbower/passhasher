1cbe8fe1f6716d323c8909e85e935f33296a8e12
Daniel Bower <daniel.bower@ecommerce.com>
2011-01-02 14:24:18 -0500

    add simple test for users controller

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "should get user page when logged in" do
+    get :show, {:user_id => users(:user_one).id}, {'user_id' => users(:user_one).id}
+    assert_response :success
+  end
+
+  test "should get blank user page when not logged in" do
+    get :show
+    assert_response :success
   end
 end
