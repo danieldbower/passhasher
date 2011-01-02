b3e89d7a0f7d217831d0616e139194d7cd675d0d
Daniel Bower <daniel.bower@ecommerce.com>
2011-01-02 14:17:16 -0500

    fix broken tests

--- test/functional/places_controller_test.rb
+++ test/functional/places_controller_test.rb
@@ -2,48 +2,23 @@ require 'test_helper'
 
 class PlacesControllerTest < ActionController::TestCase
   setup do
-    @place = places(:one)
-  end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:places)
+    @place = places(:place_one)
   end
 
   test "should get new" do
-    get :new
+    get :new, {:user_id => users(:user_one).id}, {'user_id' => users(:user_one).id}
     assert_response :success
   end
 
   test "should create place" do
     assert_difference('Place.count') do
-      post :create, :place => @place.attributes
+      post :create, {:place => @place.attributes, :user_id => users(:user_one).id}, {'user_id' => users(:user_one).id}
     end
-
-    assert_redirected_to place_path(assigns(:place))
-  end
-
-  test "should show place" do
-    get :show, :id => @place.to_param
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, :id => @place.to_param
-    assert_response :success
-  end
-
-  test "should update place" do
-    put :update, :id => @place.to_param, :place => @place.attributes
-    assert_redirected_to place_path(assigns(:place))
   end
 
   test "should destroy place" do
     assert_difference('Place.count', -1) do
-      delete :destroy, :id => @place.to_param
+      delete :destroy, {:id => @place.to_param, :user_id => users(:user_one).id}, {'user_id' => users(:user_one).id}
     end
-
-    assert_redirected_to places_path
   end
 end
