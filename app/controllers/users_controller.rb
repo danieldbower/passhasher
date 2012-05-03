0f0ee7822dbe2401b98c7eaaeaeccf3afeb7a145
Daniel Bower <daniel.d.bower@gmail.com>
2012-05-03 01:13:37 -0400

    Bye Rails

--- app/controllers/users_controller.rb
+++
@@ -1,7 +0,0 @@
-class UsersController < ApplicationController
-  def show
-    if(current_user)
-      @places = Place.places_for_user current_user
-    end
-  end
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Passhasher::Application.config.secret_token = '84cd6bdee608f574095d310ef1746278d5898acb0f78beef8c41adc676374cce18bdc2ae43f47e522b51996c68f17e0867ecc569d98f6a94bdcb4902017b4c3d'
