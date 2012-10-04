3c12b62cacdfec302e355ecb4352cf3316613696
Daniel Bower <daniel.d.bower@gmail.com>
2012-10-04 00:37:48 -0400

    update test data and todo list

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -14,25 +14,25 @@ class BootStrap {
 		Role appRole = new Role(authority:"ROLE_PASSHASHER")
 		appRole.save()
 		
-		User user1 = new User(name:"daniel", username:"daniel", password:"daniel", 
+		User user1 = new User(name:"Daniel", username:"daniel", password:"daniel", 
 			enabled:true, accountExpired:false, accountLocked:false, passwordExpired:false)
 		user1.save()
 		UserRole user1Role = new UserRole(user:user1, role:appRole)
 		user1Role.save()
 		
-		User user2 = new User(name:"ariel", username:"ariel", password:"ariel", 
+		User user2 = new User(name:"Ariel", username:"ariel", password:"ariel", 
 			enabled:true, accountExpired:false, accountLocked:false, passwordExpired:false)
 		user2.save()
 		UserRole user2Role = new UserRole(user:user2, role:appRole)
 		user2Role.save()
 		
-		User user3 = new User(name:"david", username:"david", password:"david", 
+		User user3 = new User(name:"David", username:"david", password:"david", 
 			enabled:true, accountExpired:false, accountLocked:false, passwordExpired:false)
 		user3.save()
 		UserRole user3Role = new UserRole(user:user3, role:appRole)
 		user3Role.save()
 		
-		User user4 = new User(name:"samuel", username:"samuel", password:"samuel", 
+		User user4 = new User(name:"Samuel", username:"samuel", password:"samuel", 
 			enabled:true, accountExpired:false, accountLocked:false, passwordExpired:false)
 		user4.save()
 		UserRole user4Role = new UserRole(user:user4, role:appRole)
